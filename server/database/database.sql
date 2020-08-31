d9df50ca75a0acc8e829c29482a4024efd773929
Cholago <cholawilson@yahoo.com>
2020-08-31 13:29:17 +0300

    second commit

---
+++ server/database/database.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_status` enum('Y','N') NOT NULL DEFAULT 'N',
+  `token_code` varchar(100) NOT NULL,
+  `date_created` varchar(100) NOT NULL,
+  `date_updated` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
