f94e8ad66c5ac6f2d7004d4f3c90653966b098a3
Cholago <cholawilson@yahoo.com>
2020-09-06 01:25:54 +0300

    fourth commit

--- app/src/containers/navigationBar.jsx
+++ app/src/containers/navigationBar.jsx
@@ -9,7 +9,6 @@ const mapStateToProps = state => ({
 })
 
 const NavigationBar = ({ userInfo, authState, dispatch }) => {
-    console.log('Current', userInfo)
     return (
         <header>
             <div className="nav-bar-container">
